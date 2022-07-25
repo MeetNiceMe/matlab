79ce810063aeb25b5f2c27bf0cc52daadb5a846b
Handsome_Boy <67263670+MeetNiceMe@users.noreply.github.com>
2022-07-25 09:05:39 +0800

    Add files via upload

---
+++ 基于区域生长的肝影像分割系统/login.m
@@ -0,0 +1,134 @@
+function varargout = login(varargin)
+% LOGIN MATLAB code for login.fig
+%      LOGIN, by itself, creates a new LOGIN or raises the existing
+%      singleton*.
+%
+%      H = LOGIN returns the handle to a new LOGIN or the handle to
+%      the existing singleton*.
+%
+%      LOGIN('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in LOGIN.M with the given input arguments.
+%
+%      LOGIN('Property','Value',...) creates a new LOGIN or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before login_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to login_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help login
+
+% Last Modified by GUIDE v2.5 28-May-2022 17:05:30
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @login_OpeningFcn, ...
+                   'gui_OutputFcn',  @login_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before login is made visible.
+function login_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to login (see VARARGIN)
+
+% Choose default command line output for login
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes login wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = login_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+
+function accnum_Callback(hObject, eventdata, handles)
+% hObject    handle to accnum (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of accnum as text
+%        str2double(get(hObject,'String')) returns contents of accnum as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function accnum_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to accnum (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function root_Callback(hObject, eventdata, handles)
+% hObject    handle to root (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of root as text
+%        str2double(get(hObject,'String')) returns contents of root as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function root_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to root (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+accnum=get(handles.accnum,'string');
+root=get(handles.root,'string');
+if accnum=="admin"&&root=="admin"
+    close(untitled); %关闭界?，登录界?的?件名，不加后缀名
+    set(untitled,'Visible','on'); %打开界?，主界?的?件名，不加后缀名
+else
+    msgbox(sprintf('登录失败'), '提示信息', 'modal');
+end
---
+++ 基于区域生长的肝影像分割系统/untitled.m
@@ -0,0 +1,220 @@
+function varargout = untitled(varargin)
+% UNTITLED MATLAB code for untitled.fig
+%      UNTITLED, by itself, creates a new UNTITLED or raises the existing
+%      singleton*.
+%
+%      H = UNTITLED returns the handle to a new UNTITLED or the handle to
+%      the existing singleton*.
+%
+%      UNTITLED('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in UNTITLED.M with the given input arguments.
+%
+%      UNTITLED('Property','Value',...) creates a new UNTITLED or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before untitled_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to untitled_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help untitled
+
+% Last Modified by GUIDE v2.5 08-Jun-2022 14:12:18
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @untitled_OpeningFcn, ...
+                   'gui_OutputFcn',  @untitled_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before untitled is made visible.
+function untitled_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to untitled (see VARARGIN)
+
+% Choose default command line output for untitled
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes untitled wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = untitled_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+I=handles.I;
+X = imadjust(I, [0.2 0.8], [0 1]);
+% 区域生长分割
+X = im2double(im2uint8(mat2gray(X)));
+[J, seed_point, ts] = Regiongrowing(X);
+handles.seed_point=seed_point;
+figure(1);
+axes(handles.axes1); imshow(I, []);
+hold on;
+plot(seed_point(1), seed_point(2), 'ro', 'MarkerSize', 10, 'MarkerFaceColor', 'r');
+title('种子点选择');
+hold off;
+axes(handles.axes2);  imshow(J, []); title('区域生长分割结果');
+guidata(hObject, handles);
+
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+[FileName,PathName,FilterIndex] = uigetfile({'*.jpg;*.tif;*.png;*.gif', ...
+    '所有图像文件';...
+    '*.*','所有文件' },'载入数字图像',...
+    '.\\images\\手写数字\\t0.jpg');
+if isequal(FileName, 0) || isequal(PathName, 0)
+    return;
+end
+fileName = fullfile(PathName, FileName);
+I = imread(fileName);
+imshow(I, [], 'parent', handles.axes3); title('原图');
+handles.I = I;
+set(handles.popupmenu1,'string',PathName);
+%set(handles.text11,'string',PathName);
+guidata(hObject, handles);
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+I = handles.I;
+X = imadjust(I, [0.2 0.8], [0 1]);
+% 阈值分割
+bw = im2bw(X, graythresh(X));
+[r, c] = find(bw);
+rect = [min(c) min(r) max(c)-min(c) max(r)-min(r)];
+Xt = imcrop(X, rect);
+% 自动获取种子点
+seed_point = handles.seed_point;
+% 区域生长分割
+X = im2double(im2uint8(mat2gray(X)));
+X(1:rect(2), :) = 0;
+X(:, 1:rect(1)) = 0;
+X(rect(2)+rect(4):end, :) = 0;
+X(:, rect(1)+rect(3):end) = 0;
+[J, seed_point, ts] = Regiongrowing(X, seed_point);
+% 形态学后处理
+bw = imfill(J, 'holes');
+bw = imopen(bw, strel('disk', 5));
+% 提取边缘
+ed = bwboundaries(bw);
+axes(handles.axes4); imshow(bw, []); title('形态学后处理图像');
+axes(handles.axes5); imshow(I); 
+hold on;
+for k = 1 : length(ed)
+    % 边缘
+    boundary = ed{k};
+    plot(boundary(:,2), boundary(:,1), 'g', 'LineWidth', 2);
+end
+hold off;
+title('边缘标记图像');
+
+
+% --- Executes on button press in pushbutton4.
+function pushbutton4_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+[FileName,PathName] =  uiputfile({'*.jpg;*.tif;*.png;*.gif','All Image Files';...
+    '*.*','All Files' },'Save Image',...
+    fullfile(pwd, 'screen.jpg'));
+if isequal(FileName, 0) || isequal(PathName, 0)
+    return;
+end
+f = getframe(gcf);
+f = frame2im(f);
+imwrite(f, fullfile(PathName, FileName));
+msgbox('保存成功！', '提示信息', 'modal');
+
+
+% --- Executes on button press in pushbutton5.
+function pushbutton5_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+choice = questdlg('确定要退出系统?', ...
+    '退出', ...
+    '确定','取消','取消');
+switch choice
+    case '确定'
+        close;
+    case '取消'
+        return;
+end
+
+
+% --- Executes on button press in pushbutton6.
+function pushbutton6_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton6 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+I=handles.I;
+bw = im2bw(I, graythresh(I));
+axes(handles.axes6); imshow(bw, []); title('阈值分割图像');
+
+
+% --- Executes on selection change in popupmenu1.
+function popupmenu1_Callback(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu1 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from popupmenu1
+
+
+% --- Executes during object creation, after setting all properties.
+function popupmenu1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
